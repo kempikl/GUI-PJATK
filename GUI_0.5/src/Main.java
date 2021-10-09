b78146a221d7ff90a65c1db0c00bbf2d6a9fecf1
Lukasz <kempikl@gmail.com>
2021-10-10 01:21:51 +0200

    Added GUI_0.5

--- GUI_0.5/src/Main.java
+++ GUI_0.5/src/Main.java
@@ -1,6 +1,17 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+
+        int[] tab;
+        BubbleSort bs = new BubbleSort();
+
+        IntStream generator = new Random().ints(100, 10, 101);
+        tab = generator.toArray();
+
+        bs.bubbleSort(tab);
+        bs.printArray(tab);
     }
 }
